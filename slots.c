400db06b557fd808fef57fc3a96735078b34c7e3
Amando Rivera <riveraa6@my.erau.edu>
2024-11-13 21:40:01 -0700

    Update slots.c
    -Still working on file i/o
    -still working on harder version once user accrues enough money

--- slots.c
+++ slots.c
@@ -6,6 +6,7 @@ File name: slots.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 void yellow()     ///paint casino text yellow
    {
     printf("\033[0;33m"); 
@@ -14,35 +15,22 @@ void red()       ///paint top and bottom of casino sign red
    {
     printf("\033[0;31m");
    }
-void sign()     ///print casino sign
-    {
-  red();
-  printf(" ABCEGF  KGE  IHG  IFL  FGK  GE   GE   EE  JGF  KFI  EHF  EGL  FGECDG\n");      
-  printf("AP   LHQPIGFS GGEROHFKN EGJOPFFOK FEQMOFFP HHEROKGHN EHFSRFHJL HL   IG\n");     
-  printf("B JGGF                                                          AHKKMJ\n");   
-  yellow();
-  printf("                                    XXX                         \n");   
-  printf("       NQPNNM     QDAA     AAQOOON  MLA  FAEN  LAD    QPNQQZ    \n");    
-  printf("      QA        AA    MQ   HG            IIIQQ  FH  NHA   DDA   \n");   
-  printf("      QA        EA    AQ   ADCBA    ADA  IIO AAHIH  QMC   HKC   \n");       
-  printf("      QB        EFXXXXXA     ADDA   FLC  IIF QIJJH  PJA   HLC   \n");
-  printf("      QC        AA    AQ        HB  BFA  IIF  QQHH  MQQ   IHA   \n");
-  printf("       AAAAAAW  QQ    AQ  OMAAAAAA  OQQ  CAA    AB    IAAAAS   \n");     
-  red();
-  printf("A JK                                                             NFJIG\n");
-  printf("B IIBA                                                          AHMN L\n");    
-  printf("AQ   NF  IEA  EFD  FDJ  CEI  DD Z CC   CD  NFC  JDE  CFE  CGH  FM   OA\n"); 
-  printf(" ADDFHE  KIF  HHG  JGM  FIL  GF R FF VZGF  KGG ZMGH  GGG  GFLW FGDDB \033[0m\n"); 
-    }
 
-int betting(int bet)
+void reset()       ///paint top and bottom of casino sign red
+   {
+    printf("\033[0m");
+   }
+int betting()
 {                      ///Prompts the user on the bets they want to wager for each slot turn
+  int bet = 10;
   char input;
-  printf("Welcome to the slots! How much would you like to bet?\n");
+  printf("\nWelcome to the slots! How much would you like to bet?\n");
   while(bet >= 0){
-    printf("--------------------------------------------");
+    printf("--------------------------------------------\n");
     printf("\nInstructions: \nEnter + for +$10 bet or enter - for -$10 bet\nEnter 1 to play or enter q to leave slots\n");
-    printf("--------------------------------------------");
+    printf("\n--------------------------------------------\n");
+    printf("Bonuses double bets, and jackpots are 10x bets");
+    printf("\n--------------------------------------------");
     printf("\nBet amount: $%d\n", bet);
     scanf(" %c", &input);
     if (input == '+')
@@ -65,100 +53,175 @@ int betting(int bet)
     else {
         printf("Invalid input. Please try again.\n");
          }  
+    system("clear");     
   }
   return bet;
 } 
 
-int slots(int amountOfBet)
+void move()
+{
+   int x,i;
+   system("clear");
+   printf("--------------------------------------\n");
+   printf("|1111111  111    111111 1111 1111111 |\n");
+   printf("|111      111    11  11  11  111     |\n");
+   printf("|1111111  111    11  11  11  1111111 |\n");
+   printf("|    111  11111  11  11  11      111 |\n");
+   printf("|1111111  11111  111111  11  1111111 |\n");
+   printf("--------------------------------------\n");
+   for(x=0; x<3; x++)
+      {
+       
+       sleep(1);
+       printf("||  1  1   ||     1   ||  1      ||\n");
+       printf("||  1  1   ||  1  1   ||  1  1   ||\n");
+       printf("||  1      ||  1  1   ||  1      ||\n");
+       printf("||     1   ||     1   ||     1   ||\n");
+       printf("--------------------------------------\n");
+       
+       }
+       
+     
+}
+
+int slotsEasy(int amountOfBet, int money)
 {              ///Each slot has independent chances of occurring where 3 of the same garners a bonus
   char input;
-  int x, y, z; 
-  int money = 200;
+  int x, turn=0; 
   while (money > 0)
-  {
-    srand(time(NULL));        /// A player wins the jackpot if they get three 7's in a row
-    int icon1 = (rand()% 100) + 1;
-    int icon2 = (rand()% 50) + 3;
-    int icon3 = (rand()% 80) + 2;
-    printf("\nBalance $%d", money);
-    printf("\nEnter 1 to spin: ");
-    scanf(" %c", &input);
-    if (input == '1')
-    {
-       money = money - amountOfBet;
-       switch(icon1){
-    case 1 ... 50:
-      printf("1: Cherry\n");
-      x = 1;
-      break;
+  {        /// A player wins the jackpot if they get three 7's in a row                                           
+    printf("\nBalance $%d. Bets of $%d", money, amountOfBet);
+    printf("\nEnter 1 to spin or q to exit: ");
+    scanf(" %1c", &input);
+    if (input == '1') 
+    {  
+      turn++;
+      money -= amountOfBet;
+      move();
+      system("clear");
+      printf("--------------------------------------\n");
+      printf("|1111111  111    111111 1111 1111111 |\n");
+      printf("|111      111    11  11  11  111     |\n");
+      printf("|1111111  111    11  11  11  1111111 |\n");
+      printf("|    111  11111  11  11  11      111 |\n");
+      printf("|1111111  11111  111111  11  1111111 |\n");
+      for(x=0; x<3; x++)
+      {
+        
+        printf("--------------------------------------\n");
+        int icon1 = (rand()% 100) + 1; 
+        switch(icon1) {
+            case 1 ... 2:
+                yellow();
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("|| 1   111 || 1   111 || 1   111 ||\n");
+                printf("||     111 ||     111 ||     111 ||\n");
+                printf("||    1111 ||    1111 ||    1111 ||\n");
+                reset();
+                money = money + (amountOfBet*10);
+                break;
+            
+            case 3 ... 8:
+                yellow();
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("||  11111  ||  11111  ||  11111  ||\n");
+                printf("||   111   ||   111   ||   111   ||\n");
+                printf("||    1    ||    1    ||    1    ||\n");
+                reset();
+                money = money + (amountOfBet*5);
+                break;
+            
+            case 9 ... 17:
+                yellow();
+                printf("|| 1  1  1 || 1  1  1 || 1  1  1 ||\n");
+                printf("|| 11 1 11 || 11 1 11 || 11 1 11 ||\n");
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                reset();
+                money = money + (amountOfBet*2);
+                break;
+            
+            case 18 ... 28:
+                yellow();
+                printf("||   111   ||   111   ||   111   ||\n");
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("||    1    ||    1    ||    1    ||\n");
+                reset();
+                money = money + (amountOfBet*2);
+                break;
     
-    case 51 ... 90:
-      printf("1: Apple\n");
-      x = 2;
-      break;
+            case 29 ... 44:
+                yellow();
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("|| 111 111 || 111 111 || 111 111 ||\n");
+                printf("|| 11   11 || 11   11 || 11   11 ||\n");
+                printf("|| 11   11 || 11   11 || 11   11 ||\n");
+                reset();
+                money = money + (amountOfBet*2);
+                break;
     
-    default:
-      printf("1:7\n");
-      x = 3;
-      break;         
+            case 45 ... 55:
+                printf("|| 1111111 || 1111111 || 1111111 ||\n");
+                printf("|| 111 111 || 1   111 || 111 111 ||\n");
+                printf("|| 11   11 ||     111 || 11   11 ||\n");
+                printf("|| 11   11 ||    1111 || 11   11 ||\n");
+                break;
+    
+            case 56 ... 65:
+                printf("|| 1  1  1 || 1111111 || 1111111 ||\n");
+                printf("|| 11 1 11 || 1   111 ||  11111  ||\n");
+                printf("|| 1111111 ||     111 ||   111   ||\n");
+                printf("|| 1111111 ||    1111 ||    1    ||\n");
+                break;
+    
+            case 66 ... 75:
+                printf("|| 1111111 || 1111111 ||   111   ||\n");
+                printf("|| 1   111 ||  11111  || 1111111 ||\n");
+                printf("||     111 ||   111   || 1111111 ||\n");
+                printf("||    1111 ||    1    ||    1    ||\n");
+                break;
+    
+            case 76 ... 85:
+                printf("||   111   || 1111111 || 1111111 ||\n");
+                printf("|| 1111111 || 1   111 || 111 111 ||\n");
+                printf("|| 1111111 ||     111 || 11   11 ||\n");
+                printf("||    1    ||    1111 || 11   11 ||\n");
+                break;
+    
+            case 86 ... 90:
+                printf("|| 1  1  1 || 1  1  1 || 1111111 ||\n");
+                printf("|| 11 1 11 || 11 1 11 || 111 111 ||\n");
+                printf("|| 1111111 || 1111111 || 11   11 ||\n");
+                printf("|| 1111111 || 1111111 || 11   11 ||\n");
+                break;
+    
+            default:
+                printf("|| 1111111 ||   111   || 1111111 ||\n");
+                printf("|| 1   111 || 1111111 || 1   111 ||\n");
+                printf("||     111 || 1111111 ||     111 ||\n");
+                printf("||    1111 ||    1    ||    1111 ||\n");
+                break;
+       
       }
-  
-  switch(icon2){
-    case 3 ... 20:
-      printf("2: Cherry\n");
-      y = 1;
-      break;
-  
-    case 21 ... 40:
-      printf("2: Apple\n");
-      y = 2;
-      break;
-  
-    default:
-      printf("2: 7\n");
-      y = 3;
-      break;
-                }
-  switch(icon3){
-    case 2 ... 40:
-      printf("3: Cherry\n");
-      z = 1;
-      break;
-    
-    case 41 ... 70:
-      printf("3: Apple\n");
-      z = 2;
-      break;
-    
-    default:
-      printf("3:7\n");
-       z = 3;
-       break;         
-                }
-  if ((z==y) & (x==y) && x != 3){
-    yellow();
-    printf("You have won a bonus!\033[0m\n");
-    money = money + 30;
- }else if((z==y) & (x==y) && x == 3){
-    yellow();
-    printf("You have won the jackpot!\033[0m\n");
-    money = money + 100;
- } 
     }
+}
     else if (input == 'q')
-    {
-        printf("Goodbye");
-    }
+      {
+        printf("Goodbye\n");
+        break;
+      }
   }
     return money;
- 
 }
+  
 
 
 int main()
-{                                                                                                                     
-  sign();
-  slots(betting(10));
+{                                                                      
+
+  srand(time(NULL));
+  slotsEasy(betting(), 200);
 	return 0;
 }
 
